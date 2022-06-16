586c1e41e25c8da6629750dcd9cfc046bdc6fc40
yyh <413935740@qq.com>
2022-06-16 19:13:26 +0800

    修改个人设置

--- app/src/main/java/com/schoolnews/manage/application/constant/AddressContants.java
+++ app/src/main/java/com/schoolnews/manage/application/constant/AddressContants.java
@@ -10,7 +10,7 @@ public final class AddressContants {
 
     static {
         if (GlobalConfigContants.intEnviSwitch == 0) {
-            SERVER_ADDR_HOME = "http://192.168.0.109:8086";//测试环境使用
+            SERVER_ADDR_HOME = "http://192.168.0.114:8086";//测试环境使用
 //            SERVER_ADDR_HOME = "http://192.168.0.5:8080";
         }
     }
@@ -22,12 +22,12 @@ public final class AddressContants {
     public static final String API_SERVER_MODIFY_MSG = SERVER_ADDR_HOME + "/user/register";
     public static final String API_SERVER_REGIST = SERVER_ADDR_HOME + "/user/register";
     public static final String API_SERVER_FEE_LIST = SERVER_ADDR_HOME + "/news/collectionList";
-    public static final String API_SERVER_COMMON_LIST = SERVER_ADDR_HOME + "/news/commentList";  //文件上传
-    public static final String API_SERVER_ADD_COMMON_LIST = SERVER_ADDR_HOME + "/news/addComment";  //文件上传
-    public static final String API_SERVER_COLLECT = SERVER_ADDR_HOME + "/news/addCollection";  //文件上传
-    public static final String API_SERVER_CANCEL_COLLECT = SERVER_ADDR_HOME + "/news/deleteCollection";  //文件上传
-    public static final String API_SERVER_LIKE = SERVER_ADDR_HOME + "/admin/file/upload";  //文件上传
-    public static final String API_SERVER_NO_LIKE = SERVER_ADDR_HOME + "/admin/file/upload";  //文件上传
+    public static final String API_SERVER_COMMON_LIST = SERVER_ADDR_HOME + "/news/commentList";
+    public static final String API_SERVER_ADD_COMMON_LIST = SERVER_ADDR_HOME + "/news/addComment";
+    public static final String API_SERVER_COLLECT = SERVER_ADDR_HOME + "/news/addCollection";
+    public static final String API_SERVER_CANCEL_COLLECT = SERVER_ADDR_HOME + "/news/deleteCollection";
+    public static final String API_SERVER_LIKE = SERVER_ADDR_HOME + "/admin/file/upload";
+    public static final String API_SERVER_NO_LIKE = SERVER_ADDR_HOME + "/admin/file/upload";
 
 
     public static final String API_SERVER_ZHAUNZHANG_LIST = SERVER_ADDR_HOME + "/news/list";
